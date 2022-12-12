c1e88a483dae63f42c4c9f46b4a33a8132305f56
NeduSki <chipro262@gmail.com>
2022-12-12 15:43:41 +0100

    initial commit

---
+++ EXAMINATION/answers.js
@@ -0,0 +1,15 @@
+//Question 4
+
+//4i - Adding a new car at the beginning of the array.
+const car = ["Mercedes", "Toyota","Honda","Ferarri"]
+car.unshift("Dodge")
+console.log(car)
+
+//4ii - Returning the third element of the array.
+var car = ["Mercedes", "Toyota","Honda","Ferarri"]
+thirdElement = car(2)
+console.log(thirdElement);
+
+//4iii - Removing the last element from the array. 
+var car = ["Mercedes", "Toyota","Honda","Ferarri"]
+console.log(car.pop());
